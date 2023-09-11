9f63c4ec2ff3a0fa6a91b6e571e2b1662ec1a466
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-09-11 17:16:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+let multiply = num1 * num2
+
+let random = 1
+let num3 = 13
+let num4 = 9
+
+let mod = num3 % num4
+
+let max = Math.max(20, 1)
